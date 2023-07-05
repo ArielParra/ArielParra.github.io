da005e7dc1923e1781467f897757870807cc34a9
Ariel Parra <al280862@edu.uaa.mx>
2023-07-05 10:22:37 -0600

    lang y boton quitado

--- language.js
+++ language.js
@@ -1,34 +1,20 @@
+var currentlanguage;
+if (navigator.language.startsWith('es')) {
+    currentlanguage = 'es';
+    console.log('default lang es');
+}else{
+    currentlanguage = 'en';
+    console.log('default lang en');
+}
+changeLanguage(currentlanguage);
 
- 
-    var currentlanguage;
-    if (navigator.language.startsWith('es')) {
-        currentlanguage = 'es';
-        console.log('default lang es');
-    } else {
-        currentlanguage = 'en';
-        console.log('default lang en');
-
-    }
-    changeLanguage(currentlanguage);
-    
-    function langbutton(){
-        if (currentlanguage=='es') {
-            currentlanguage = 'en';
+function changeLanguage(currentlanguage) {
+    Array.from(document.getElementsByClassName('lang')).forEach(function(elem){
+        if (elem.classList.contains(currentlanguage)) {
+            elem.style.display = 'initial';
+        }else{
+            elem.style.display = 'none';
         }
-        else {
-            currentlanguage = 'es';
-        }
-    changeLanguage(currentlanguage);
-    }
-
-    function changeLanguage(currentlanguage) {
-        Array.from(document.getElementsByClassName('lang')).forEach(function (elem) {
-            if (elem.classList.contains(currentlanguage)) {
-                 elem.style.display = 'initial';
-            }
-            else {
-                 elem.style.display = 'none';
-            }
-        });
-      }
+    });
+}
 
